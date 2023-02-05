8cbb0b754a3df6c43fc2fd2fea26d34bd5210da3
Cipto Hartanto <cipto.hartanto@yahoo.com>
2023-02-06 00:01:30 +0800

    ✨ ssr

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,8 +1,15 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
+  ssrMode: typeof window === 'undefined',
   uri: process.env.HYGRAPH,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    query: {
+      fetchPolicy: 'no-cache',
+      errorPolicy: 'all',
+    },
+  },
 });
 
 export default client;
